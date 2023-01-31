d1cb01fe546300a753b8fb3e8441b2501ddae31b
Павел Жилин <username@example.com>
2023-01-31 13:49:04 +0500

    Final header sidebar.

--- burger_menu.js
+++ burger_menu.js
@@ -1,11 +1,10 @@
 jQuery('document').ready(function () {
 
-	var cvs, left_css;
-	cvs = document.getElementsByClassName( "burger_menu" ); // подключаем полотно для отображения кода
+	var burger;
+	burger = document.getElementsByClassName( "menu_icon" ); // подключаем полотно для отображения кода
 
-	jQuery(cvs).on("click", function() {
+	jQuery(burger).on("click", function() {
 		jQuery(".sidebar").toggleClass( "active");
-		console.log("click!");
 	});
 
 });
